4e59c263331ea49696654c91f1ff7a8783aacaea
cindyjiang <cindyj301@gmail.com>
2021-07-14 14:35:52 -0500

    working on reply methods

--- aa_questions.rb
+++ aa_questions.rb
@@ -14,6 +14,11 @@ end
 class User 
     attr_accessor :lname, :fname
 
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM users")
+        data.map { |datum| User.new(datum) }
+    end
+
     def self.find_by_id(id)
         user = QuestionDBConnection.instance.execute(<<-SQL, id)
             SELECT
@@ -45,15 +50,45 @@ class User
         @fname = options['fname']
         @lname = options['lname']
     end
+
+    def create
+        raise "#{self} is already in database" if id
+        QuestionDBConnection.instance.execute(<<-SQL, fname, lname)
+        INSERT INTO
+            users (fname, lname)
+        VALUES
+            (?, ?);
+        SQL
+        id = QuestionDBConnection.instance.last_insert_row_id
+    end
+
+    def authored_questions
+        Question.find_by_author_id(id)
+    end
+
+    def authored_replies
+        Reply.find_by_user_id(id)
+    end
 end
 
 class Question
     attr_accessor :id, :title, :body, :author_id
-    def initialize(options)
-         @id = options['id']
-         @title = options['title']
-         @body = options['body']
-         @author_id = options['author_id']
+
+    def self.all
+        data = QuestionDBConnection.instance.execute("SELECT * FROM questions")
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def self.find_by_author_id(author_id)
+        question = QuestionDBConnection.instance.execute(<<-SQL, author_id)
+            SELECT
+              *
+            FROM 
+              questions
+            WHERE
+              author_id = ?
+        SQL
+        question.map{|question| Question.new(question)}
     end
 
     def self.find_by_id(id)
@@ -69,20 +104,41 @@ class Question
         User.new(question.first)
     end
 
-    def self.find_by_author_id(author_id)
-        question = QuestionDBConnection.instance.execute(<<-SQL, author_id)
-            SELECT
-              *
-            FROM 
-              questions
-            WHERE
-              author_id = ?
-        SQL
-        question.map{|question| Question.new(question)}
+    def initialize(options)
+         @id = options['id']
+         @title = options['title']
+         @body = options['body']
+         @author_id = options['author_id']
     end
 
-    
+    def create
+        raise "#{self} already in database" if self.id
+        QuestionDBConnection.instance.execute(<<-SQL, self.title, self.body, self.author_id)
+        INSERT INTO
+            questions (title, body, author_id)
+        VALUES
+            (?, ?, ?)
+        SQL
+        self.id = QuestionDBConnection.instance.last_insert_row_id
+    end
 
+    def author
+        result = QuestionDBConnection.instance.execute(<<-SQL, author_id)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                id = ?
+        SQL
+        return nil unless result.length > 0
+        author = User.new(result.first)
+        author.fname + ' ' + author.lname
+    end
+
+    def replies
+        Reply.find_by_question_id(id)
+    end
 end
 
 class QuestionFollow
@@ -130,10 +186,67 @@ class Reply
         return nil unless reply.length > 0
         User.new(reply.first)
     end
+
+    def find_by_user_id(user_ref_id)
+        replies = QuestionDBConnection.instance.execute(<<-SQL, user_ref_id)
+            SELECT
+              *
+            FROM 
+              replies
+            WHERE
+              user_ref_id = ?
+        SQL
+        replies.map { |reply| Reply.new(reply) }
+    end
+
+    def find_by_question_id(question_ref_id)
+        replies = QuestionDBConnection.instance.execute(<<-SQL, question_ref_id)
+            SELECT
+                *
+            FROM 
+                replies
+            WHERE
+                question_ref_id = ?
+        SQL
+        replies.map { |reply| Reply.new(reply) }
+    end
+
+    def author
+        result = QuestionDBConnection.instance.execute(<<-SQL, user_ref_id)
+        SELECT
+            *
+        FROM
+            users
+        WHERE
+            id = ?
+        SQL
+        return nil unless result.length > 0
+        author = User.new(result.first)
+        author.fname + ' ' + author.lname
+    end
+
+    def question
+        question = QuestionDBConnection.instance.execute(<<-SQL, question_ref_id)
+        SELECT
+            *
+        FROM
+            questions
+        WHERE
+            id = ?
+        SQL
+        return nil unless question.length > 0
+        Question.new(question.first)
+    end
+
+    def parent_reply
+    end
+
+    def child_replies
+    end
 end
 
 class QuestionLike
-    attr_accessor :id, :likes,:user_like_ref_id ,:quesiton_like_ref_id
+    attr_accessor :id, :likes,:user_like_ref_id ,:question_like_ref_id
 
      def initialize(options)
          @id = options['id']
